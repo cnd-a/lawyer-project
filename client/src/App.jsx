ccc803705c0a9c7a729ca5559bd1dd93a105488d
Bruh <191955524+pura-toru@users.noreply.github.com>
2025-09-08 18:13:25 +0700

    Refactor lines and convert App function to arrow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,42 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import "./App.css";
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import dotenv from 'dotenv'
+import './App.css';
 
-// Import all pages at once
-import Home from "./pages/Home/Home.jsx";
-import Login  from "./pages/Authentication/Login.jsx";
-import Register from "./pages/Authentication/Register.jsx";
-import Splash from "./pages/SplashScreen/SplashScreen.jsx";
-import Lawyers from "./pages/Lawyers/Lawyers.jsx";
-import CreateLawyer from "./pages/CreateLawyer/CreateLawyer.jsx";
-import ArticleList from "./pages/ArticleList/ArticleList.jsx";
-import Article from "./components/Article/Article.jsx";
-import CreateArticle from "./pages/CreateArticle/CreateArticle.jsx";
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+import Home from './pages/Home/Home.jsx';
+import Login  from './pages/Authentication/Login.jsx';
+import Register from './pages/Authentication/Register.jsx';
+import Splash from './pages/SplashScreen/SplashScreen.jsx';
+
+import Lawyers from './pages/Lawyers/Lawyers.jsx';
+import CreateLawyer from './pages/CreateLawyer/CreateLawyer.jsx';
+
+import ArticleList from './pages/ArticleList/ArticleList.jsx';
+import Article from './components/Article/Article.jsx';
+import CreateArticle from './pages/CreateArticle/CreateArticle.jsx';
 
 // Route configArticle  
 const routes = [
-  { path: "/", element: <Splash /> },
-  { path: "/register", element: <Register /> },
-  { path: "/login", element: <Login /> },
+  { path: '/', element: <Splash /> },
+  { path: '/register', element: <Register /> },
+  { path: '/login', element: <Login /> },
 
-  { path: "/home", element: <Home /> },
+  { path: '/home', element: <Home /> },
 
-  { path: "/lawyers", element: <Lawyers /> },
-  { path: "/lawyers/create", element: <CreateLawyer /> },
+  { path: '/lawyers', element: <Lawyers /> },
+  { path: '/lawyers/create', element: <CreateLawyer /> },
 
-  { path: "/articles", element: <ArticleList /> },
-  { path: "/articles/:id", element: <Article /> },
-  { path: "/articles/create", element: <CreateArticle /> },
+  { path: '/articles', element: <ArticleList /> },
+  { path: '/articles/:id', element: <Article /> },
+  { path: '/articles/create', element: <CreateArticle /> },
 ];
 
 // Pages that show header/footer
-const withLayout = ["/home", "/lawyers", "/articles"];
+const withLayout = ['/home', '/lawyers', '/articles'];
 
-function App() {
+const App = () => {
   const location = useLocation();
   const showLayout = withLayout.some((path) =>
     location.pathname.startsWith(path)
